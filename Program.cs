6a0bfe1ecffebd871a9329d1ce385020877f3d12
aawaheed <arbab@aawaheed.com>
2022-06-22 00:57:26 +0300

    Unit: InMemory DB

--- Program.cs
+++ Program.cs
@@ -1,37 +1,83 @@
 using Microsoft.OpenApi.Models;
-using PizzaStore.DB;
+using Microsoft.EntityFrameworkCore;
+using PizzaStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
     
+#region Services
+
+builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
      c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API", Description = "Making the Pizzas you love", Version = "v1" });
 });
-    
+
+#endregion Services
+
 var app = builder.Build();
-    
+
+#region Use
+
+app.UseHttpsRedirection();        
+
 if (app.Environment.IsDevelopment())
 {
      app.UseDeveloperExceptionPage();
 }
-    
+
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
 });
-    
-app.MapGet("/", () => "Hello World!");
+
+#endregion Use
+
+//To auto-redirect to swagger
+app.MapGet("/", () => Results.Redirect("/swagger/"));    
+//app.MapGet("/", () => "Hello World!");
 
 #region Pizza API
 
-app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
-app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
-app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
-app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
-app.MapDelete("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
+//To get all pizzas 
+app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
+
+//To save the incoming pizza 
+app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) =>
+{
+    await db.Pizzas.AddAsync(pizza);
+    await db.SaveChangesAsync();
+    return Results.Created($"/pizza/{pizza.Id}", pizza);
+});
+
+//To get the single pizza 
+app.MapGet("/pizza/{id}", async (PizzaDb db, int id) => await db.Pizzas.FindAsync(id));
+
+//To update the single pizza 
+app.MapPut("/pizza/{id}", async (PizzaDb db, Pizza updatepizza, int id) =>
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null) return Results.NotFound();
+    pizza.Name = updatepizza.Name;
+    pizza.Description = updatepizza.Description;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+//To delete the pizza
+app.MapDelete("/pizza/{id}", async (PizzaDb db, int id) =>
+{
+  var pizza = await db.Pizzas.FindAsync(id);
+  if (pizza is null)
+  {
+    return Results.NotFound();
+  }
+  db.Pizzas.Remove(pizza);
+  await db.SaveChangesAsync();
+  return Results.Ok();
+});
 
 #endregion Pizza API    
 
-app.Run();
+app.Run("http://localhost:3000/");
--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 `` dotnet new web -o PizzaStore ``
 ## Add swagger
 `` dotnet add package Swashbuckle.AspNetCore --version 6.2.3``
-
+## Add EF Core
+`` dotnet add package Microsoft.EntityFrameworkCore.InMemory --version 6.0 ``
